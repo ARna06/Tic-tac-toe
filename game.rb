6383258c8b7a8623f02b3eb0ad734f01a5da2bd5
ARna06 <72038543+Leopard005537@users.noreply.github.com>
2024-02-19 01:03:32 +0530

    Add display

--- game.rb
+++ game.rb
@@ -1,10 +1,28 @@
 class GameZone
-  attr_accessor :player1_Move, :player2_Move
-  def initialize(player1_Move, player2_Move)
-    @player1_Move = player1_Move
-    @player2_Move = player2_Move
+  attr_accessor :playerMove
+
+  @@board = "1|2|3\n4|5|6\n7|8|9\n"
+  @@order = 0
+
+  def initialize(playerMove)
+    @playerMove = playerMove
+    if @@order.even?
+      @@board.sub!(@playerMove, "O")
+    else
+      @@board.sub!(@playerMove, "X")
+    end
+    @@order += 1
   end
 
-  def board
+  def showBoard
+    puts "The current situation of the board is \n"
+    puts "#{@@board}"
   end
+end
+
+for x in 0...2
+  puts "Enter your move: \t"
+  move = gets.chomp
+  situation = GameZone.new(move)
+  situation.showBoard
 end
