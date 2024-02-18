d804cafc92f0ad5e30da64aad5e9f594c430579b
ARna06 <72038543+Leopard005537@users.noreply.github.com>
2024-02-18 23:48:49 +0530

    Initial Commit

---
+++ game.rb
@@ -0,0 +1,10 @@
+class GameZone
+  attr_accessor :player1_Move, :player2_Move
+  def initialize(player1_Move, player2_Move)
+    @player1_Move = player1_Move
+    @player2_Move = player2_Move
+  end
+
+  def board
+  end
+end
