dc66ea331f86154c0f03274cea6d50b99a7d9abc
ARna06 <72038543+Leopard005537@users.noreply.github.com>
2024-02-19 17:21:32 +0530

    Add logics

--- game.rb
+++ game.rb
@@ -3,15 +3,26 @@ class GameZone
 
   @@board = "1|2|3\n4|5|6\n7|8|9\n"
   @@order = 0
+  @@no_repeater = []
 
   def initialize(playerMove)
     @playerMove = playerMove
+
+    if @@no_repeater.include?(playerMove)
+      puts "The location is already occupied!"
+      puts "Choose another location"
+      return
+    else
+      @@no_repeater.append(@playerMove)
+    end
+
     if @@order.even?
       @@board.sub!(@playerMove, "O")
     else
       @@board.sub!(@playerMove, "X")
     end
     @@order += 1
+
   end
 
   def showBoard
@@ -20,8 +31,21 @@ class GameZone
   end
 end
 
-for x in 0...2
-  puts "Enter your move: \t"
+class GameLogics
+  attr_accessor :board
+
+  def initialize(board)
+    @board = board
+    @game_array = @board.split(/['|', '\n']/)
+  end
+
+  def self.column
+
+  end
+end
+
+for x in 0...6
+  puts "Enter your move: "
   move = gets.chomp
   situation = GameZone.new(move)
   situation.showBoard
